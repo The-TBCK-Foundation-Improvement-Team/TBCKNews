3cdde294b5bfba1e7b266619a962cd68fdf4d81c
Nick-Paxton <npaxton@student.neumont.edu>
2025-01-16 15:43:17 -0700

    Add functionality for News Cards to be clicked

--- tbck-times-react/src/components/NewsCards/MainNewsCard.js
+++ tbck-times-react/src/components/NewsCards/MainNewsCard.js
@@ -1,34 +1,39 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, Typography, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 export function MainNewsCard({ imgSrc, imgAlt, title, author, date }) {
     return (
-        <Card className="main-news-card news-card">
-            <CardMedia
-                component="img"
-                image={imgSrc}
-                alt={imgAlt}
-            />
-            <CardContent>
-                <Typography variant="h5" component="div">
-                    {title}
-                </Typography>
-                <Box display="flex" justifyContent="space-between">
-                    <Typography variant="body2" color="text.secondary">
-                        {author}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {new Date(date).toLocaleString('en-US', {
-                            month: 'short',
-                            day: 'numeric',
-                            year: 'numeric',
-                            hour: 'numeric',
-                            minute: 'numeric',
-                            hour12: true
-                        })}
-                    </Typography>
-                </Box>
-            </CardContent>
-        </Card>
+        <Box className="main-news-box">
+            <Link to="/placeholder" style={{ textDecoration: 'none', display: 'block' }}>
+                <Card className="news-card">
+                    <CardMedia
+                        component="img"
+                        image={imgSrc}
+                        alt={imgAlt}
+                    />
+                    <CardContent>
+                        <Typography variant="h5" component="div">
+                            {title}
+                        </Typography>
+                        <Box display="flex" justifyContent="space-between">
+                            <Typography variant="body2" color="text.secondary">
+                                {author}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {new Date(date).toLocaleString('en-US', {
+                                    month: 'short',
+                                    day: 'numeric',
+                                    year: 'numeric',
+                                    hour: 'numeric',
+                                    minute: 'numeric',
+                                    hour12: true
+                                })}
+                            </Typography>
+                        </Box>
+                    </CardContent>
+                </Card>
+            </Link>
+        </Box>
     );
 }
