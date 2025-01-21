1ee39ae08b13bf86b8d37a829544db259fae11fd
AlyssaCRippel <Alyssacrippel@gmail.com>
2025-01-21 13:52:25 -0700

    created news object

---
+++ news-service/src/main/java/com/tbck/news_service/news_service/News.java
@@ -0,0 +1,66 @@
+package com.tbck.news_service.news_service;
+
+import java.util.UUID;
+
+public class News {
+
+    private UUID newsId;
+    private String title;
+    private String content;
+    private String author;
+    private String date;
+    private String category;
+    private String image;
+    private String template;
+
+    public UUID getNewsId() {
+        return newsId;
+    }
+    public void setNewsId(UUID newsId) {
+        this.newsId = newsId;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public String getImage() {
+        return image;
+    }
+    public void setImage(String image) {
+        this.image = image;
+    }
+    public String getTemplate() {
+        return template;
+    }
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    
+}
