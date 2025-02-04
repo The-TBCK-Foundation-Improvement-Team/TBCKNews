9942aba9b3a7941b7c83830e666758d64cca4225
Nick-Paxton <npaxton@student.neumont.edu>
2025-02-04 13:28:17 -0700

    Reformat Home Page

--- tbck-times-react/src/pages/HomePage.js
+++ tbck-times-react/src/pages/HomePage.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { MainNewsCard } from '../components/news-cards/MainNewsCard';
-import { SecondaryNewsCard } from '../components/news-cards/SecondaryNewsCard';
-import { TitleNewsCard } from '../components/news-cards/TitleNewsCard';
 import axios from 'axios';
 import '../css/HomePage.css';
 
 export function HomePage() {
     const [news, setNews] = useState([]);
+    const [warriorOfTheMonth, setWarriorOfTheMonth] = useState([]);
 
     useEffect(() => {
         axios.get('http://tbck-newsapi-env.eba-ci6mgvkp.us-east-2.elasticbeanstalk.com/news')
@@ -15,60 +14,40 @@ export function HomePage() {
 
     return (
         <div className='home-page'>
-            {!news.length ? <p></p> : 
-                <div className='news-grid'>
-                    <div className='left-column-cards'>
-                        <MainNewsCard 
+            <div className="latest-news">
+                <h1 id="latest-news-header">Latest News</h1>
+                {!news.length ? <p></p> : 
+                    <div className="latest-news-grid">
+                        <MainNewsCard
                             className='news-grid-item'
-                            imgSrc={news[0].images[0].url} 
-                            imgAlt={news[0].title} 
-                            title={news[0].title} 
-                            author={news[0].author} 
-                            date={news[0].date} 
+                            title={news[0].title}
+                            imgSrc={news[0].images[0].url}
+                            imgAlt={news[0].images[0].altText}
+                            author={news[0].author}
+                            date={news[0].date}
                         />
                         <MainNewsCard
                             className='news-grid-item'
-                            imgSrc={news[1].images[0].url}
-                            imgAlt={news[1].title}
                             title={news[1].title}
+                            imgSrc={news[1].images[0].url}
+                            imgAlt={news[1].images[0].altText}
                             author={news[1].author}
                             date={news[1].date}
                         />
-                    </div>
-                    <div className='center-column-cards'>
                         <MainNewsCard
                             className='news-grid-item'
-                            imgSrc={news[2].images[0].url}
-                            imgAlt={news[2].title}
                             title={news[2].title}
+                            imgSrc={news[2].images[0].url}
+                            imgAlt={news[2].images[0].altText}
                             author={news[2].author}
                             date={news[2].date}
                         />
-                        <TitleNewsCard className='news-grid-item' title={news[3].title} />
-                        <TitleNewsCard className='news-grid-item' title={news[4].title} />
                     </div>
-                    <div className='right-column-cards'>
-                        <SecondaryNewsCard
-                            className='news-grid-item'
-                            imgSrc={news[5].images[0].url}
-                            imgAlt={news[5].title}
-                            title={news[5].title}
-                        />
-                        <SecondaryNewsCard
-                            className='news-grid-item'
-                            imgSrc={news[6].images[0].url}
-                            imgAlt={news[6].title}
-                            title={news[6].title}
-                        />
-                        <SecondaryNewsCard
-                            className='news-grid-item'
-                            imgSrc={news[7].images[0].url}
-                            imgAlt={news[7].title}
-                            title={news[7].title}
-                        />
-                    </div>
-                </div>
-            }
+                }
+            </div>
+            <div className="warrior-of-the-month-hp">
+
+            </div>
         </div>
     );
 }
