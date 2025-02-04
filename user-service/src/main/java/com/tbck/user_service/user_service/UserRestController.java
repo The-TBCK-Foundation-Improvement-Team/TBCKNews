2f4f7b214824e864ccb4e3b7344d7381ba733ba2
AlyssaCRippel <Alyssacrippel@gmail.com>
2025-02-04 15:12:43 -0700

    fixed user roles setting in js

--- user-service/src/main/java/com/tbck/user_service/user_service/UserRestController.java
+++ user-service/src/main/java/com/tbck/user_service/user_service/UserRestController.java
@@ -1,10 +1,10 @@
 package com.tbck.user_service.user_service;
 
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,8 +20,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.servlet.http.HttpServletResponse;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 import software.amazon.awssdk.services.dynamodb.model.DeleteItemRequest;
@@ -118,10 +116,10 @@ public class UserRestController {
         user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
 
         // Ensure verification field is set
-        if (user.getVerified() == null) {
+        if (user.getVerified() == null || user.getVerified() == false) {
             user.setVerified(false);
         }
-        if (user.getRole() == null) {
+        if (user.getRole() == null || user.getRole().isEmpty()) {
             user.setRole("GUEST");
         }
 
