2c0ecd1170091cfa4acc5fb00f4b398024871556
AlyssaCRippel <142117512+AlyssaCRippel@users.noreply.github.com>
2025-02-20 16:47:47 -0700

    warrior page alsmost done

--- tbck-times-react/src/pages/WarriorOfTheMonth.js
+++ tbck-times-react/src/pages/WarriorOfTheMonth.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../css/WarriorOfTheMonth.css';
 import '../css/GenericNews.css';
 
@@ -8,19 +8,51 @@ import { MuiCategoryBar } from '../components/MuiCategoryBar.js';
 import { MuiCommentBox } from '../components/news-components/MuiCommentBox.js'
 import { MuiLikeButton } from '../components/news-components/MuiLikeButton.js'
 import { MuiSuggestedStories } from "../components/SuggestedStories.js";
+import ImageSlideShow from '../components/ImageSlideshow.js';
 
 function WarriorOfTheMonth({title, contentOne, contentTwo, contentThree, images}) {
+    const contents = [contentOne, contentTwo, contentThree];
+    
+    const splitTitle = title.split(' '); // Split title into words
+    const lastWord = splitTitle.pop(); // Get the last word
+    const remainingTitle = splitTitle.join(' '); // Join the rest of the title back into a string
+
     return (
-        // <p>Warrior of the Month</p>
-        <div className='container'>      
-            <div className="news-layout">
-                
+        <div className='warrior-layout'>      
+        <div className="warrior-container">
+            <div className="content-layout">
+            <h1 className='warrior-title'>{remainingTitle} <span className='pink-name'>{lastWord}</span></h1>
+            {contents.map((content, index) => (
+                        <div key={index} className="content-item">
+                            {/* Conditionally alternating the image and content side */}
+                            {index % 2 === 0 ? (
+                                <>
+                                    {/* If it's the 1st, 2nd or 3rd content and there are enough images, display slideshow */}
+                                    {index === contents.length - 1 && contents.length > 1 && (
+                                        <ImageSlideShow images={images} />
+                                    )}
+                                    {images[index].url && index !== contents.length - 1 && (
+                                        <img src={images[index].url} alt={`image-${index}`} className="content-image" />
+                                    )}
+                                    {content && <div className="content-text content-one">{content}</div>}
+                                </>
+                            ) : (
+                                <>
+                                    {content && <div className="content-text content-two">{content}</div>}
+                                    {images[index].url && index !== contents.length - 1 && (
+                                        <img src={images[index].url} alt={`image-${index}`} className="content-image" />
+                                    )}
+                                </>
+                            )}
+                        </div>
+                    ))}
+                </div>
             </div>
-            <div className='sidebar-layout'>
-                <MuiSuggestedStories />
-            </div>
-            <MuiFooter/>
+        <div className='sidebar-layout'>
+            <MuiSuggestedStories />
         </div>
+        <MuiFooter />
+    </div>
     );
 }
 
