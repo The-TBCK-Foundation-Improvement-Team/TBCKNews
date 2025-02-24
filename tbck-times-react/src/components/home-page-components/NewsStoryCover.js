3655bcc899c49b6e160eb3a7cccd837c7b7350a5
Nick-Paxton <npaxton@student.neumont.edu>
2025-02-24 16:35:06 -0700

    Home page latest news cards and section complete

--- tbck-times-react/src/components/home-page-components/NewsStoryCover.js
+++ tbck-times-react/src/components/home-page-components/NewsStoryCover.js
@@ -6,8 +6,10 @@ export function NewsStoryCover() {
     return (
         <div className="news-story-cover">
             <Link to="/GenericNews" style={{ textDecoration: 'none', display: 'block' }}>
-                <img className="news-story-cover-image" src="https://static.vecteezy.com/system/resources/thumbnails/001/950/054/small_2x/newspaper-mockup-template-free-vector.jpg" alt="News Story Cover" />
-                <p className="news-story-cover-blurb">Small blurb</p>
+                <div className="news-story-cover-image-container">
+                    <img className="news-story-cover-image" src="https://static.vecteezy.com/system/resources/thumbnails/001/950/054/small_2x/newspaper-mockup-template-free-vector.jpg" alt="News Story Cover" />
+                    <p className="news-story-cover-blurb">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+                </div>
                 <div className="news-story-cover-content">
                     <h2 className="news-story-cover-title">News Story Title</h2>
                     <div className="news-story-cover-author-date">
