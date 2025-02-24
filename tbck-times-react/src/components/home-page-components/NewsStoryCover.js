ad09de305ce666f4b24381fce8c93a27b48b8233
Nick-Paxton <npaxton@student.neumont.edu>
2025-02-24 14:39:47 -0700

    Latest news cards mostly working

--- tbck-times-react/src/components/home-page-components/NewsStoryCover.js
+++ tbck-times-react/src/components/home-page-components/NewsStoryCover.js
@@ -7,12 +7,14 @@ export function NewsStoryCover() {
         <div className="news-story-cover">
             <Link to="/GenericNews" style={{ textDecoration: 'none', display: 'block' }}>
                 <img className="news-story-cover-image" src="https://static.vecteezy.com/system/resources/thumbnails/001/950/054/small_2x/newspaper-mockup-template-free-vector.jpg" alt="News Story Cover" />
-                <h2 className="news-story-cover-title">News Story Title</h2>
                 <p className="news-story-cover-blurb">Small blurb</p>
-                <div className="news-story-cover-author-date">
-                    <p>Author</p>
-                    <p>|</p>
-                    <p>Date</p>
+                <div className="news-story-cover-content">
+                    <h2 className="news-story-cover-title">News Story Title</h2>
+                    <div className="news-story-cover-author-date">
+                        <p>Author</p>
+                        <p>|</p>
+                        <p>Date</p>
+                    </div>
                 </div>
             </Link>
         </div>
