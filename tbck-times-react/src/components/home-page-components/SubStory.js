5193b6a9dfaad68f6c181e51ea7e9fa8d9b9c04a
Nick-Paxton <npaxton@student.neumont.edu>
2025-02-25 14:02:35 -0700

    Added warrior of the month section to the home page

--- tbck-times-react/src/components/home-page-components/SubStory.js
+++ tbck-times-react/src/components/home-page-components/SubStory.js
@@ -8,7 +8,7 @@ export function SubStory() {
             <Link to="/GenericNews" style={{ textDecoration: 'none', display: 'block' }}>
                 <div className="sub-story-content">
                     <img className="sub-story-image" src="https://static.vecteezy.com/system/resources/thumbnails/001/950/054/small_2x/newspaper-mockup-template-free-vector.jpg" alt="News Story Cover" />
-                    <p className="sub-story-title">News Story Title</p>
+                    <p className="sub-story-title">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
                 </div>
             </Link>
         </div>
--- tbck-times-react/src/components/home-page-components/WotMSection.js
+++ tbck-times-react/src/components/home-page-components/WotMSection.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import '../../css/home-page-components/WotMSection.css';
+import { SubStory } from './SubStory';
 
 export function WotMSection() {
 
     return (
         <div className="hp-wotm-section">
             <div className="hp-wotm-left-section">
-                <img src="/images/WotM.png" alt="Warrior of the Month" />
+                <img src="/images/WotM.png" alt="Warrior of the Month" className="hp-wotm-title-img" />
+                <div className="hp-wotm-left-section-content">
+                    <div className="hp-wotm-left-section-images">
+                        <img className="hp-wotm-main-img" src="https://tbck-news-images.s3.us-east-2.amazonaws.com/tbck-news-image/Annie1.png" alt="Warrior of the Month" />
+                        <div className="hp-wotm-sub-imgs">
+                            <img className="hp-wotm-sub-img" src="https://tbck-news-images.s3.us-east-2.amazonaws.com/tbck-news-image/Annie2.png" alt="Warrior of the Month" />
+                            <img className="hp-wotm-sub-img" src="https://tbck-news-images.s3.us-east-2.amazonaws.com/tbck-news-image/Annie3.png" alt="Warrior of the Month" />
+                            <img className="hp-wotm-sub-img" src="https://tbck-news-images.s3.us-east-2.amazonaws.com/tbck-news-image/Annie4.png" alt="Warrior of the Month" />
+                        </div>
+                    </div>
+                    <div className="hp-wotm-left-section-text">
+                        <h1>Name/Family</h1>
+                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. <br/>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. <br/>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. <br/>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
+                    </div>
+                </div>
             </div>
             <div className="hp-wotm-right-section">
-
+                <h2>Past Articles</h2>
+                <SubStory />
+                <SubStory />
+                <SubStory />
+                <SubStory />
             </div>
         </div>
     );
