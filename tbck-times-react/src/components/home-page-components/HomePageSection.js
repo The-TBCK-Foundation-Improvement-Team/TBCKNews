3c38bbf254cb99b02f17b85c58d81ac3f5686388
Nick-Paxton <npaxton@student.neumont.edu>
2025-02-26 14:17:33 -0700

    Home page is close to complete, just need more dummy data

--- tbck-times-react/src/components/home-page-components/HomePageSection.js
+++ tbck-times-react/src/components/home-page-components/HomePageSection.js
@@ -1,18 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { NewsStoryCover } from './NewsStoryCover';
 import { SubStory } from './SubStory';
 import '../../css/home-page-components/HomePageSection.css';
 
 export function HomePageSection({ title, id }) {
+    const [news, setNews] = useState([]);
 
-    
+    useEffect(() => {
+        axios.get('http://newsserviceapi-env.eba-kaahc5te.us-east-2.elasticbeanstalk.com/news/category/'+ title)
+            .then((response) => setNews(response.data));
+    }, []);
 
     return (
-        <div className='home-page-section' id={id}>
-            <h1>{title}</h1>
-            <NewsStoryCover />
-            <SubStory />
-            <SubStory />
-        </div>
+        <>
+            {news.length > 0 ? <div className='home-page-section' id={id}>
+                <h1>{title}</h1>
+                <NewsStoryCover
+                    title={news[0].title ?? ""}
+                    author={news[0].author ?? ""}
+                    date={news[0].date ?? ""}
+                    blurb={news[0].contentOne.split(".")[0] + "." ?? ""}
+                    imgSrc={news[0].images[0] ? news[0].images[0].url : "https://static.vecteezy.com/system/resources/thumbnails/001/950/054/small_2x/newspaper-mockup-template-free-vector.jpg"}
+                    imgAlt={news[0].images[0] ? news[0].images[0].altText : ""}
+                    newsId={news[0].newsId ?? ""}
+                />
+                <SubStory />
+                <SubStory />
+            </div> : <></>}
+        </>
     );
 }
